87e0f368d7e207907fca439a2a7bc3cca4a9d04e
Michael Farwell <whitewolf9909@gmail.com>
2019-02-22 06:55:02 -0500

    Format properly

--- Ruby/day_one_bonus.rb
+++ Ruby/day_one_bonus.rb
@@ -2,12 +2,12 @@ correct_guess = false
 correct_num = rand(10)
 
 until correct_guess
-  puts 'What is your guess?'
+  puts "What is your guess?"
   user_guess = gets.to_i
   if user_guess == correct_num
-    puts 'You guessed correct!'
+    puts "You guessed correct!"
     correct_guess = true
   else
-    puts 'You guess wrong!'
+    puts "You guess wrong!"
   end
 end
